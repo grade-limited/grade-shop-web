7a3bcd054514d6fafc79730c47fe78db081cc62b
tam11a <ibrahimsadiktamim@gmail.com>
2023-12-11 00:21:30 +0600

    category grid done

--- src/components/pages/home/Category/index.tsx
+++ src/components/pages/home/Category/index.tsx
@@ -1,12 +1,30 @@
 import { previewImage } from "@/service";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Category: React.FC<{ categories?: any[] }> = ({ categories }) => {
-	console.log(categories);
 	return (
-		<div className="grid grid-cols-4">
-			{categories?.map((category) => {
-				return <div key={category.id}>{category.name}</div>;
+		<div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-4 relative">
+			{categories?.map((category, index) => {
+				return (
+					<Link
+						key={index}
+						href={`/category/${category.id}`}
+						className={`h-40 p-4 rounded-md relative ${
+							category.color_code ? category.color_code : "bg-slate-200"
+						}`}
+					>
+						<Image
+							src={previewImage(category.thumbnail_url)}
+							height={120}
+							width={120}
+							className="absolute bottom-0 right-0 h-full w-auto"
+							alt={category.name}
+						/>
+						<span className="font-bold relative pl-2">{category.name}</span>
+					</Link>
+				);
 			})}
 		</div>
 	);
