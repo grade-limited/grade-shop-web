9030f0522216de63a6b94cd54a2c608d21721803
tam11a <ibrahimsadiktamim@gmail.com>
2024-01-04 20:11:27 +0600

    cart done

--- src/layouts/components/Cart/OrderCart.tsx
+++ src/layouts/components/Cart/OrderCart.tsx
@@ -1,7 +1,63 @@
+import Iconify from "@/components/iconify";
+import { findUnitPrice } from "@/pages/product/[id]";
+import { useGetCarts } from "@/queries/cart";
+import { previewImage } from "@/service";
+import {
+	Avatar,
+	Collapse,
+	List,
+	ListItemAvatar,
+	ListItemButton,
+	ListItemSecondaryAction,
+	ListItemText,
+	ListSubheader,
+} from "@mui/material";
+import { Skeleton } from "antd";
 import React from "react";
 
 const OrderCart: React.FC = () => {
-	return <div>OrderCart</div>;
+	const { data, isLoading } = useGetCarts();
+
+	return (
+		<>
+			<List
+				dense
+				className="flex-1"
+			>
+				<ListSubheader>Personal</ListSubheader>
+				{isLoading ? (
+					<Skeleton />
+				) : (
+					data?.map?.((item: any) => (
+						<ListItemButton key={item.id}>
+							<ListItemAvatar>
+								<Avatar
+									variant="rounded"
+									className="border-slate-200 border bg-slate-200 text-slate-600"
+									src={previewImage(item.product.thumbnail_url)}
+								>
+									<Iconify icon={"solar:album-broken"} />
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText
+								primary={item.product.name}
+								secondary={`${findUnitPrice(
+									"bb2e",
+									1,
+									item.product.price
+								)}৳ × ${item.quantity} items = ${
+									findUnitPrice("bb2e", 1, item.product.price) * item.quantity
+								}৳`}
+							/>
+							<ListItemSecondaryAction>
+								<Iconify icon={"solar:pen-broken"} />
+							</ListItemSecondaryAction>
+						</ListItemButton>
+					))
+				)}
+			</List>
+		</>
+	);
 };
 
 export default OrderCart;
--- src/layouts/components/Cart/index.tsx
+++ src/layouts/components/Cart/index.tsx
@@ -2,17 +2,26 @@ import Iconify from "@/components/iconify";
 import { findUnitPrice } from "@/pages/product/[id]";
 import { useGetCarts } from "@/queries/cart";
 import { useAuth } from "@/service/auth";
+import { DialogTitle, Drawer, IconButton } from "@mui/material";
 import React from "react";
+import OrderCart from "./OrderCart";
+import { useToggle } from "@tam11a/react-use-hooks";
+import { Icon } from "@iconify/react";
+import { Divider } from "antd";
 
 const Cart: React.FC = () => {
 	const { data, isLoading } = useGetCarts();
 	const { isAuthenticated, isValidationLoading } = useAuth();
+	const { state, toggleState } = useToggle(false);
 
 	if (isLoading || !isAuthenticated || isValidationLoading) return null;
 
 	return (
 		<div>
-			<div className="absolute z-[12121212121] top-1/3 right-0 -translate-y-1/2 bg-slate-800 p-3 flex flex-row items-center justify-center rounded-l shadow-md shadow-slate-900 gap-2 border-r-0 cursor-pointer">
+			<div
+				className="absolute z-[1500] top-1/3 right-0 -translate-y-1/2 bg-slate-800 p-3 flex flex-row items-center justify-center rounded-l shadow-md shadow-slate-900 gap-2 border-r-0 cursor-pointer"
+				onClick={toggleState}
+			>
 				<Iconify
 					icon={"solar:cart-3-bold"}
 					className="text-4xl text-primary-600"
@@ -31,6 +40,36 @@ const Cart: React.FC = () => {
 					</p>
 				</div>
 			</div>
+			<Drawer
+				open={state}
+				onClose={toggleState}
+				anchor="right"
+				className="z-[1501]"
+				PaperProps={{
+					className: "w-[95vw] max-w-sm",
+				}}
+			>
+				<DialogTitle className="flex flex-row items-center justify-between">
+					<h3>Cart {!!data?.length && <>({data?.length})</>}</h3>
+					<IconButton>
+						<Icon icon={"mdi:close"} />
+					</IconButton>
+				</DialogTitle>
+				<Divider className="my-0" />
+				<OrderCart />
+				<Divider className="my-0" />
+				<div className="flex flex-row items-center justify-between p-4">
+					<h3>Total</h3>
+					<p className="font-semibold">
+						{data
+							?.map?.((item: any) =>
+								findUnitPrice("bb2e", item.quanitty, item.product.price)
+							)
+							?.reduce?.((x: number, y: number) => x + y) || 0}
+						৳
+					</p>
+				</div>
+			</Drawer>
 		</div>
 	);
 };
